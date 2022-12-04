653401cd70e89b1e33f5cb614da59592fea1ef7a
Patrick Li <admin@aocrec.com>
2022-12-04 16:09:36 +0800

    mgxparser 0.3.1

--- test.js
+++ test.js
@@ -3,14 +3,16 @@
 const analyzer = require('./mgxnode');
 const { resolve } = require('path');
 
-const absolute_path = resolve('/Workspace/mgxnode/libs/MgxParser/test/test_records/AOC10_4v4_3_192a8268.zip');
+//const absolute_path = resolve('/Workspace/mgxnode/libs/MgxParser/test/test_records/AOC10_4v4_3_192a8268.zip');
+const absolute_path = resolve('/Workspace/mgxhub/tmpsave/837b5135760be3309273fa5ecc680b63.zip');
 const rec = new analyzer({
     input: absolute_path,
     map: true,
     hd: true,
     mapWidth: 600,
     mapHeight: 300,
-    unzip: 'buffer'
+    //unzip: 'buffer',
+    md5: false
     // header: 'testheader.dat',
     // body: 'testbody.dat',
 });
@@ -18,7 +20,7 @@ const rec = new analyzer({
 const rec_info = rec.result();
 const rec_map_base64 = rec.map_base64();
 //rec.savemap('testmap.png');
-rec.unzipsave()
+//rec.unzipsave()
 
 console.log(rec_info);
 //console.log(rec_map_base64);
